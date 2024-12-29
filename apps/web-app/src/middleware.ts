ca82cb530cba5e31345724faff6a957679de9e90
Chris Watts <chris.watts.t@gmail.com>
2024-12-28 17:32:52 -0800

    feat: try clerk hook again

--- apps/web-app/src/middleware.ts
+++ apps/web-app/src/middleware.ts
@@ -4,6 +4,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
+  "/api/webhooks/clerk",
   "/api/webhooks/clerk(.*)",
 ]);
 
