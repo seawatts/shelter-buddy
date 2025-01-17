9fc485cfebe42f11ea0adc4e450bb93a921f4098
Chris Watts <chris.watts.t@gmail.com>
2025-01-17 13:33:43 -0800

    fix: rever db client to vercel postgres for now

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,12 +1,14 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
 
-import { env } from "./env";
 import * as schema from "./schema";
 
+// import postgres from "postgres";
+
 // For migrations
-export const migrationClient = postgres(env.POSTGRES_URL, { max: 1 });
+// export const migrationClient = postgres(env.POSTGRES_URL, { max: 1 });
 
 // For query purposes
-export const queryClient = postgres(env.POSTGRES_URL);
-export const db = drizzle(queryClient, { schema });
+// export const queryClient = postgres(env.POSTGRES_URL);
+// export const db = drizzle(queryClient, { schema });
+export const db = drizzle(sql, { schema });
